1204cdef60158b76b879d0b1fa84afe86ec47ec0
Nacho Varela <nachouve@gmail.com>
2013-05-20 19:04:33 +0200

    Minor changes to improve test

--- index.php
+++ index.php
@@ -5,27 +5,24 @@ require 'vendor/autoload.php';
 require_once('Database.php');
 require_once('Geojson.php');
 
-set_time_limit(40);
-
-$db = new Database();
-$json_conv = new GeoJSON();
-
 $app = new \Slim\Slim();
 
+# Test function
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
+    exit(0);
 });
 
 $app->get('/features', function (){
 
     include('config.php');
-    global $db;
-    global $json_conv;
+    $db = new Database();
     #echo "<h1>Feature All </h1>";
     $resp = $db->getAll();
    # while ($r = $resp->fetch()){
    #   print_r($r);
    # }
+    $json_conv = new GeoJSON();
     $a = $json_conv->createJson($resp->fetchAll(PDO::FETCH_ASSOC), $TBL_X, $TBL_Y);
     header('Content-type: application/json');
     #echo '<p>['.json_encode($a, JSON_NUMERIC_CHECK).']</p>';
@@ -36,13 +33,13 @@ $app->get('/features', function (){
 $app->get('/feature/:id', function ($id){
 
     include('config.php');
-    global $db;
-    global $json_conv;
+    $db = new Database();
     #echo "<h1>Feature $id </h1>";
     $resp = $db->getID($id);
    # while ($r = $resp->fetch()){
    #   print_r($r);
    # }
+    $json_conv = new GeoJSON();
     $a = $json_conv->createJson($resp->fetchAll(PDO::FETCH_ASSOC), $TBL_X, $TBL_Y);
     header('Content-type: application/json');
     #echo '<p>['.json_encode($a, JSON_NUMERIC_CHECK).']</p>';

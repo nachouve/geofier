bcc2850c71a2370eec2182019f866aef8cf3cb74
Concha Alonso <conchalonso@gmail.com>
2013-11-03 13:32:11 +0100

    Some styles changed in main

--- public_html/templates/main.php
+++ public_html/templates/main.php
@@ -2,29 +2,50 @@
 {% block content %}
 
 <div class="jumbotran jumbo_welcome">
-	<div class="container container_extra_narrow">
-		<h2 class="font_econ">Welcome to <a href="http://nuve.hol.es/geofier/">Geofier</a></h2>
+	<div class="container">
+				<div class="row">
+			<div class="col-lg-1">
+			</div>
+			<div class="col-lg-11">
+				<h2 class="font_econ">Welcome to <a style="color:orange" href="http://nuve.hol.es/geofier/">Geofier</a></h2>
+			</div>
+					</div>
+
+
 	</div><!-- /container -->
 </div><!-- /jumbotron -->
 
 <div class="jumbotran jumbo_two">
-	<div class="container container_extra_narrow">
-		<div class="row">
-			<div class="col-lg-6">
-				Go to the <a href="api" class="btn btn-warning" role="button">API</a>
+	<div class="container">
+				<div class="row" style="font-family: 'Arapey'; font-size:1.2em">
+			<div class="col-lg-1">
 			</div>
-			<div class="col-lg-6">
-				Learn more <a href="api" class="btn btn-warning" role="button">ABOUT</a> this project
+			<div class="col-lg-8">
+				Go to the <a href="api" class="btn btn-default" role="button">API</a> or learn more <a href="api" class="btn btn-default" role="button">ABOUT</a> this project
 			</div>
+
 		</div>
 
 	</div><!-- /container -->
 
 </div><!-- /jumbotron -->
-<footer>
-	<p id="version">
+
+<div class="jumbotran jumbo_welcome">
+	<div class="container">
+				<div class="row">
+			<div class="col-lg-1">
+			</div>
+			<div class="col-lg-11">
+				<p id="version">
 		Running Geofier version 0.2.
 	</p>
-</footer>
+			</div>
+					</div>
+
+
+	</div><!-- /container -->
+</div><!-- /jumbotron -->
+
+
 
 {% endblock content %}
